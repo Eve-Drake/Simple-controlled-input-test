0e00464fda2326a5b1f16d8963f7c6f7d7bbd48f
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-09-27 18:30:05 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [colour, setColour] = useState<string>('')
+
+  //Simple Regex to remove spaces in inputs -- eg. 'light blue' to 'lightblue'
+  const colourNoSpaces = colour.replace(/\s/g, '')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+      placeholder='Enter a Colour to Change the Box'
+      value={colour}
+      onChange={(e)=>setColour(e.target.value)}
+      />
+
+      {/*Area For Styling */}
+      <div className='colourDisplay' style={{backgroundColor: `${colourNoSpaces}`}} />
     </div>
   );
 }
